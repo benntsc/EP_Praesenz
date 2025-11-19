0ab9ec0331cd0351e470199aef91900a40912720
Benn Tschierschke <benn@MacBook-Pro-von-Benn.local>
2025-11-19 12:58:34 +0100

    Uebung 5: P2

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -2,4 +2,23 @@ package p2;
 
 public class P2_main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = { 1, -8000, 5 };
+
+		int max = 0;
+		int minIndex = 0;
+
+		max = numbers[0];
+
+		for (int i = 0; i <= numbers.length - 1; i++) {
+			if (numbers[i] > max) {
+				max = numbers[i];
+				minIndex = i;
+			}
+		}
+		System.out.println("Max: " + max);
+		System.out.println("Mininmum Index: " + minIndex);
+	}
+
 }
