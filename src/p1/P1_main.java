9393389d0b5c6d214d676140956ecbd2f3c3544e
Benn Tschierschke <benn@MacBook-Pro-von-Benn.local>
2025-11-19 12:33:25 +0100

    Uebung5: P1 neu

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -7,20 +7,20 @@ public class P1_main {
 		for (int i = 1; i <= 11; i++) {
 			System.out.println(i);
 		}
-		
-		
-		int x=0;
-		while ( x <= 10 ) {
-		x = x + 1;
-		System.out.println(x);
+		System.out.println();
+
+		int x = 0;
+		while (x <= 10) {
+			x = x + 1;
+			System.out.println(x);
 		}
-		
-		
+		System.out.println();
+
 		int y = 0;
 		do {
 			y = y + 1;
 			System.out.println(y);
-			} while (y <= 10);
+		} while (y <= 10);
 	}
 
 }
